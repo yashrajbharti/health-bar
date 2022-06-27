a211539ebb2c678fb0fc9b3313f41260b8d8b7cf
Yash Raj <yashraj@Yashs-MacBook-Air.local>
2022-06-27 13:27:27 +0530

    health bars are draggable

--- drag.js
+++ drag.js
@@ -2,3 +2,12 @@ $('.bar').resizable({
     handles: 'e',minWidth: 200,
     maxWidth: 700
 });
+$( function() {
+    $( "#atk-bar" ).draggable();
+  } );
+  $( function() {
+    $( "#def-bar" ).draggable();
+  } );
+  $( function() {
+    $( "#hp-bar" ).draggable();
+  } );
